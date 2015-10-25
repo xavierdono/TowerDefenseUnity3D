3859f5dccf407c28feb133b1aea316766cb87d1e
xavierdono <xavier.dono@gmail.com>
2015-10-25 09:14:02 +0100

    Add Asset

---
+++ Scripts/GameManagerBehavior.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManagerBehavior : MonoBehaviour
+{
+	public Text goldLabel;
+	public Text waveLabel;
+	public Text healthLabel;
+	public GameObject[] healthIndicator;
+	public GameObject[] nextWaveLabels;
+	public bool gameOver = false;
+
+	private int gold;
+
+	public int Gold {
+		get { return gold; }
+		set {
+			gold = value;
+			goldLabel.GetComponent<Text> ().text = "GOLD : " + gold;
+		}
+	}
+
+	private int wave;
+
+	public int Wave {
+		get { return wave; }
+		set {
+			wave = value;
+
+			if (!gameOver) {
+				for (int i = 0; i < nextWaveLabels.Length; i++) {
+					nextWaveLabels [i].GetComponent<Animator> ().SetTrigger ("nextWave");
+				}
+			}
+
+			waveLabel.text = "WAVE : " + (wave + 1);
+		}
+	}
+
+	private int health;
+
+	public int Health {
+		get { return health; }
+		set {
+			if (value < health) {
+				Camera.main.GetComponent<CameraShake> ().Shake ();
+			}
+
+			health = value;
+			healthLabel.text = "HEALTH : " + health;
+
+			if (health <= 0 && !gameOver) {
+				gameOver = true;
+				GameObject gameOverText = GameObject.FindGameObjectWithTag ("GameOver");
+				gameOverText.GetComponent<Animator> ().SetBool ("gameOver", true);
+			}
+
+			for (int i = 0; i < healthIndicator.Length; i++) {
+				if (i < Health) {
+					healthIndicator [i].SetActive (true);
+				} else {
+					healthIndicator [i].SetActive (false);
+				}
+			}
+		}
+	}
+
+	void Start ()
+	{
+		Gold = 1000;
+		Wave = 0;
+		Health = 5;
+	}
+}
---
+++ Scripts/PlaceMonster.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceMonster : MonoBehaviour
+{
+	private GameManagerBehavior gameManager;
+	public GameObject monsterPrefab;
+	private GameObject monster;
+
+	void Start ()
+	{
+		gameManager = GameObject.Find ("GameManager").GetComponent<GameManagerBehavior> ();
+	}
+
+	private bool canPlaceMonster ()
+	{
+		int cost = monsterPrefab.GetComponent<MonsterData> ().levels [0].cost;
+		return monster == null && gameManager.Gold >= cost;
+	}
+
+	// Automatically calls OnMouseUp when a player taps a GameObject’s physics collider
+	void OnMouseUp ()
+	{
+		// Place free?
+		if (canPlaceMonster ()) { // Yes
+			
+			// Store the instance of the monsterPrefab who has been clicked
+			monster = (GameObject)Instantiate (monsterPrefab, transform.position, Quaternion.identity);
+
+			// Play the sound when clicked
+			AudioSource audioSource = gameObject.GetComponent<AudioSource> ();
+			audioSource.PlayOneShot (audioSource.clip);
+
+			// Deduct gold
+			gameManager.Gold -= monster.GetComponent<MonsterData> ().CurrentLevel.cost;
+
+		} else if (canUpgradeMonster ()) {
+			monster.GetComponent<MonsterData> ().increaseLevel ();
+
+			AudioSource audioSource = gameObject.GetComponent<AudioSource> ();
+			audioSource.PlayOneShot (audioSource.clip);
+
+			// Deduct gold
+			gameManager.Gold -= monster.GetComponent<MonsterData> ().CurrentLevel.cost;
+
+		}
+	}
+
+	private bool canUpgradeMonster ()
+	{
+		if (monster != null) {
+			
+			MonsterData monsterData = monster.GetComponent<MonsterData> ();
+			MonsterLevel nextLevel = monsterData.getNextLevel ();
+
+			if (nextLevel != null) {
+				return gameManager.Gold >= nextLevel.cost;
+			}
+		}
+		return false;
+	}
+}
